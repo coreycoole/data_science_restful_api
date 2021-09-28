e0f146040655ec49b41a62ca98c14c4d9126310b
Corey Coole <coreycoole@MacBook-Pro.local>
2021-09-27 20:24:58 -0700

    add basic data view and input url

--- ds_proof_of_concept/urls.py
+++ ds_proof_of_concept/urls.py
@@ -1,11 +1,12 @@
-from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
 from rest_framework import routers
-from .views import InputDataViewset
+from .views import InputDataViewset, BasicDataTestView
 
 router = routers.DefaultRouter()
-router.register("", InputDataViewset, basename="proof_of_concept")
+router.register("input", InputDataViewset, basename="proof_of_concept")
 
 urlpatterns = [
     path('', include(router.urls)),
+    url(r'^basic/$', BasicDataTestView.as_view()),
 ]
--- ds_proof_of_concept/views.py
+++ ds_proof_of_concept/views.py
@@ -1,9 +1,23 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, views
+from rest_framework.response import Response
 from .models import InputData
 from .serializers import InputDataSerializer
 
 class InputDataViewset(viewsets.ModelViewSet):
     queryset = InputData.objects.all()
     serializer_class = InputDataSerializer
+
+
+class BasicDataTestView(views.APIView):
+
+    @staticmethod
+    def get(request):
+        return Response({"status": "successful", "payload": ["Hello World"]})
+
+    @staticmethod
+    def post(input_data):
+        """find out how to post data to ML algo"""
+        return None
+
 
 
